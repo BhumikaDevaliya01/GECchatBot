25e4c2f09d417640b2e9f6491f22ab228a56b5f4
BhumikaDevaliya01 <bhumikadevaliya7@gmail.com>
2024-02-10 20:45:31 +0530

    Integrate MongoDB Database Connection

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,18 +1,3 @@
-// import express from "express";
-// import { config } from "dotenv";
-// config();
-// const app = express();
-
-// // middleweres
-// app.use(express.json());
-
-// // connections and listneres
-// app.listen(5000, () => console.log("Server Open"));
-
-
-
-
-
 import app from "./app.js";
 import connectToDatabase from "./db/connection.js";
 // connections and listneres
