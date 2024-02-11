6f0c312f65d22f8e9473158d2eee982b15502b32
BhumikaDevaliya01 <bhumikadevaliya7@gmail.com>
2024-02-11 20:23:47 +0530

    Added cookie into UserSignup

--- backend/src/controllers/user-controllers.ts
+++ backend/src/controllers/user-controllers.ts
@@ -2,6 +2,8 @@ import { NextFunction, Request, Response } from "express";
 import User from "../models/User.js";
 import { hash, compare } from "bcrypt";
 import { createToken } from "../utils/token-manager.js"; 
+import { COOKIE_NAME } from "../utils/constants.js";
+
 export const getAllUsers = async (
   req: Request,
   res: Response,
@@ -29,6 +31,26 @@ try {
     const hashedPassword = await hash(password, 10);
     const user = new User({ name, email, password: hashedPassword });
     await user.save();
+
+    // create token and store cookie
+        res.clearCookie(COOKIE_NAME, {
+          httpOnly: true,
+          domain: "localhost",
+          signed: true,
+          path: "/",
+        });
+    
+        const token = createToken(user._id.toString(), user.email, "7d");
+        const expires = new Date();
+        expires.setDate(expires.getDate() + 7);
+        res.cookie(COOKIE_NAME, token, {
+          path: "/",
+          domain: "localhost",
+          expires,
+          httpOnly: true,
+          signed: true,
+        });
+
     return res.status(200).json({message:"OK"});
   } catch (error) {
     console.log(error);
