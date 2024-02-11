123abaa9485b68202e3b1f99c2ee792ce40aca91
BhumikaDevaliya01 <bhumikadevaliya7@gmail.com>
2024-02-11 11:15:47 +0530

    Added User Signup

--- backend/src/controllers/user-controllers.ts
+++ backend/src/controllers/user-controllers.ts
@@ -1,5 +1,7 @@
 import { NextFunction, Request, Response } from "express";
 import User from "../models/User.js";
+import { hash } from "bcrypt";
+
 
 export const getAllUsers = async (
   req: Request,
@@ -10,6 +12,25 @@ export const getAllUsers = async (
     //get all users
     const users = await User.find();
     return res.status(200).json({ message: "OK", users });
+  } catch (error) {
+    console.log(error);
+    return res.status(200).json({ message: "ERROR", cause: error.message });
+  }
+};
+
+export const userSignup = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+try {
+    const { name, email, password } = req.body;
+    const existingUser = await User.findOne({ email });
+    if (existingUser) return res.status(401).send("User already registered");
+    const hashedPassword = await hash(password, 10);
+    const user = new User({ name, email, password: hashedPassword });
+    await user.save();
+    return res.status(200).json({message:"OK"});
   } catch (error) {
     console.log(error);
     return res.status(200).json({ message: "ERROR", cause: error.message });
--- backend/src/routes/user-routes.ts
+++ backend/src/routes/user-routes.ts
@@ -1,5 +1,20 @@
 import { Router } from "express";
+import { 
+    getAllUsers ,
+    userSignup
+} from "../controllers/user-controllers.js";
+
+import {
+  signupValidator,
+  validate
+} from "../utils/validators.js";
+
 
 const userRoutes = Router();
 
+userRoutes.get("/",getAllUsers);
+
+userRoutes.post("/signup",validate(signupValidator) ,userSignup);
+
 export default userRoutes;
+
