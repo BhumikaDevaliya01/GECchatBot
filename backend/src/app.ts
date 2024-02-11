dd441e555c8964f342f621db8ff44615da33d5b7
BhumikaDevaliya01 <bhumikadevaliya7@gmail.com>
2024-02-11 12:34:38 +0530

    Create Tken

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,13 +1,14 @@
-
 import express from "express";
 import { config } from "dotenv";
 import morgan from 'morgan';
 import appRouter from "./routes/index.js";
-
+import cookieParser from "cookie-parser";
 config();
 const app = express();
 
+// middleweres
 app.use(express.json());
+app.use(cookieParser(process.env.COOKIE_SECRET));
 
 app.use(morgan("dev"));
 
--- backend/src/controllers/user-controllers.ts
+++ backend/src/controllers/user-controllers.ts
@@ -1,7 +1,7 @@
 import { NextFunction, Request, Response } from "express";
 import User from "../models/User.js";
 import { hash, compare } from "bcrypt";
-
+import { createToken } from "../utils/token-manager.js";
 
 export const getAllUsers = async (
   req: Request,
@@ -53,6 +53,18 @@ export const userLogin = async (
     if (!isPasswordCorrect) {
       return res.status(403).send("Incorrect Password");
     }
+
+    const token = createToken(user._id.toString(), user.email, "7d");
+    const expires = new Date();
+    expires.setDate(expires.getDate() + 7);
+    res.cookie("COOKIE_NAME", token, {
+      path: "/",
+      domain: "localhost",
+      expires,
+      httpOnly: true,
+      signed: true,
+    });
+
     return res
       .status(200)
       .json({ message: "OK", name: user.name, email: user.email });
