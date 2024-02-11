85da438fb4a71f6adc23c1c4464efdb51f96f10d
BhumikaDevaliya01 <bhumikadevaliya7@gmail.com>
2024-02-11 12:57:52 +0530

    Create Token

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -3,12 +3,13 @@ import express from "express";
 import { config } from "dotenv";
 import morgan from 'morgan';
 import appRouter from "./routes/index.js";
+import cookieParser from "cookie-parser";
 
 config();
 const app = express();
 
 app.use(express.json());
-
+app.use(cookieParser(process.env.COOKIE_SECRET));
 app.use(morgan("dev"));
 
 app.use("/api/v1", appRouter);
