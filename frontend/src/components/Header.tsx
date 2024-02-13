cd0e9c1f912bc67c334a4ae4223ce4f8676371e1
BhumikaDevaliya01 <bhumikadevaliya7@gmail.com>
2024-02-13 11:59:19 +0530

    AppBar added

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,7 +1,14 @@
 import React from "react";
+import AppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
 
 const Header = () => {
-    return <div>Header</div>;
+    return (
+    <AppBar sx={{ bgcolor: "transparent", position: "static", boxShadow: "none" }}>
+      <Toolbar sx={{ display: "flex" }}>
+      </Toolbar>
+    </AppBar>
+    );
 }
 
 export default Header;
