6e4c584d2337e1875e1b923b228a552bbe5f2d51
BhumikaDevaliya01 <bhumikadevaliya7@gmail.com>
2024-02-17 18:55:54 +0530

    Handle submit-event of the form

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -1,8 +1,17 @@
 import React from 'react';
-import {Box, Typography} from "@mui/material";
+import {Box, Button, Typography} from "@mui/material";
 import CustomizedInput from "../components/shared/CustomizedInput";
+import { IoIosLogIn } from "react-icons/io";
+
 
 const Login = () => {
+    const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        const formData = new FormData(e.currentTarget);
+        const email = formData.get("email") as string;
+        const password = formData.get("password") as string;
+        console.log(email,password);
+      };
     return (
     <Box width={"100%"} height={"100%"} display="flex" flex={1}>
       <Box padding={8} mt={8} display={{ md: "flex", sm: "none", xs: "none" }}>
@@ -18,6 +27,7 @@ const Login = () => {
       mt={16}
       > 
         <form
+          onSubmit={handleSubmit}
           style={{
             margin: "auto",
             padding: "30px",
@@ -41,7 +51,24 @@ const Login = () => {
             >Login</Typography>
             <CustomizedInput type="email" name="email" label="Email" />
             <CustomizedInput type="password" name="password" label="Password" />
-
+            <Button
+              type="submit"
+              sx={{
+                px: 2,
+                py: 1,
+                mt: 2,
+                width: "400px",
+                borderRadius: 2,
+                bgcolor: "#00fffc",
+                ":hover": {
+                  bgcolor: "white",
+                  color: "black",
+                },
+              }}
+              endIcon={<IoIosLogIn />}
+            >
+              Login
+            </Button>
             </Box>
         </form>
       </Box>
