73f97d3eee8b69bd7717fdb9dc73ff136d8429b7
BhumikaDevaliya01 <bhumikadevaliya7@gmail.com>
2024-02-17 18:27:51 +0530

    Added Customized Input

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import {Box} from "@mui/material";
+import {Box, Typography} from "@mui/material";
+import CustomizedInput from "../components/shared/CustomizedInput";
 
 const Login = () => {
     return (
@@ -16,6 +17,33 @@ const Login = () => {
       ml={"auto"}
       mt={16}
       > 
+        <form
+          style={{
+            margin: "auto",
+            padding: "30px",
+            boxShadow: "10px 10px 20px #000",
+            borderRadius: "10px",
+            border: "none",
+          }}
+        >
+            <Box
+            sx={{
+              display: "flex",
+              flexDirection: "column",
+              justifyContent: "center",
+            }}
+            >
+              <Typography
+              variant="h4"
+              textAlign="center"
+              padding={2}
+              fontWeight={600}
+            >Login</Typography>
+            <CustomizedInput type="email" name="email" label="Email" />
+            <CustomizedInput type="password" name="password" label="Password" />
+
+            </Box>
+        </form>
       </Box>
     </Box>
     );
