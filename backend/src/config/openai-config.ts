1ae0dbaf32dc666f048814b0065d395e0d64a7e6
BhumikaDevaliya01 <bhumikadevaliya7@gmail.com>
2024-02-17 23:36:29 +0530

    openAI config

--- backend/src/config/openai-config.ts
+++ backend/src/config/openai-config.ts
@@ -1,9 +1,9 @@
-// import  Configuration from "openai";
+import  Configuration from "openai";
 
-// export const configureOpenAI = () => {
-//   const config = new Configuration({
-//     apiKey: process.env.OPEN_AI_SECRET,
-//     organization: process.env.OPENAI_ORAGANIZATION_ID,
-//   });
-//   return config;
-// };
+export const configureOpenAI = () => {
+  const config = new Configuration({
+    apiKey: process.env.OPEN_AI_SECRET,
+    organization: process.env.OPENAI_ORAGANIZATION_ID,
+  });
+  return config;
+};
