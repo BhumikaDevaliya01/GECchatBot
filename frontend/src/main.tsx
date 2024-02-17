295888ee9d39128c255508ec921e092e550218c5
BhumikaDevaliya01 <bhumikadevaliya7@gmail.com>
2024-02-17 19:32:17 +0530

    toaster added

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -5,6 +5,10 @@ import './index.css'
 import { createTheme , ThemeProvider} from '@mui/material'
 import { BrowserRouter } from "react-router-dom";
 import { AuthProvider } from "./context/AuthContext.tsx";
+import { Toaster } from 'react-hot-toast';
+import axios from "axios";
+axios.defaults.baseURL = "http://localhost:5000/api/v1";
+axios.defaults.withCredentials = true;
 
 const theme = createTheme({
   typography: {
@@ -18,6 +22,7 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
     <AuthProvider>
       <BrowserRouter>
         <ThemeProvider theme={theme}>
+        <Toaster position="top-right" />
           <App />
         </ThemeProvider>
       </BrowserRouter>
