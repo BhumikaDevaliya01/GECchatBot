95406c0a1e4cc87f0dd0f07766529bd065b74095
BhumikaDevaliya01 <bhumikadevaliya7@gmail.com>
2024-02-17 18:16:57 +0530

    Basic setup of the page

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -1,9 +1,24 @@
 import React from 'react';
+import {Box} from "@mui/material";
 
 const Login = () => {
     return (
-        <div>Login</div>
-    )
-}
+    <Box width={"100%"} height={"100%"} display="flex" flex={1}>
+      <Box padding={8} mt={8} display={{ md: "flex", sm: "none", xs: "none" }}>
+        <img src="airobot.png" alt="Robot" style={{ width: "400px" }} />
+      </Box>
+      <Box
+      display={"flex"}
+      flex={{ xs: 1, md: 0.5 }}
+      justifyContent={"center"}
+      alignItems={"center"}
+      padding={2}
+      ml={"auto"}
+      mt={16}
+      > 
+      </Box>
+    </Box>
+    );
+};
 
 export default Login;
