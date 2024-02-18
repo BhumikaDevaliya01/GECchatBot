e1c10e3cbb85d57aa7b5e47d27a0a4fbc87d8963
BhumikaDevaliya01 <bhumikadevaliya7@gmail.com>
2024-02-18 20:36:50 +0530

    Added chat

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -1,11 +1,11 @@
 import { Router } from "express";
 import userRoutes from "./user-routes.js";
-import { signupValidator, validate } from "../utils/validators.js";
-// import chatRoutes from "./chat-routes.js";
+import chatRoutes from "./chat-routes.js";
+// import { signupValidator, validate } from "../utils/validators.js";
 
 const appRouter = Router();
 
 appRouter.use("/user",userRoutes); //domain/api/v1/user
-// appRouter.use("/chats", chatRoutes);//domain/api/v1/chats
+appRouter.use("/chats", chatRoutes);//domain/api/v1/chats
 
 export default appRouter;
--- frontend/src/pages/Chat.tsx
+++ frontend/src/pages/Chat.tsx
@@ -1,9 +1,63 @@
-import React from 'react';
+import React from "react";
+import { Box, Avatar, Typography} from "@mui/material";
+import { useAuth } from "../context/AuthContext";
 
 const Chat = () => {
-    return (
-        <div>Chat</div>
-    )
-}
+  const auth = useAuth();
+
+  // return <div>chat</div>
+
+  return (
+    // <div>chat</div>
+
+    <Box
+      sx={{
+        display: "flex",
+        flex: 1,
+        width: "100%",
+        height: "100%",
+        mt: 3,
+        gap: 3,
+      }}
+    >
+      <Box
+        sx={{
+          display: { md: "flex", xs: "none", sm: "none" },
+          flex: 0.2,
+          flexDirection: "column",
+        }}
+      >
+      <Box
+          sx={{
+            display: "flex",
+            width: "100%",
+            height: "60vh",
+            bgcolor: "rgb(17,29,39)",
+            borderRadius: 5,
+            flexDirection: "column",
+            mx: 3,
+          }}
+        >
+            <Avatar
+            sx={{
+              mx: "auto",
+              my: 2,
+              bgcolor: "white",
+              color: "black",
+              fontWeight: 700,
+            }}
+            >
+              {auth?.user?.name[0]}
+              {auth?.user?.name.split("")[1][0]}
+            </Avatar>
+            <Typography sx={{ mx: "auto", fontFamily: "work sans" }}>
+            You are talking to a ChatBOT
+          </Typography>
+        </Box>
+      
+      </Box>
+    </Box>
+  );
+};
 
 export default Chat;
