9294411af140c14d82f6fa660c71c77178f49345
BhumikaDevaliya01 <bhumikadevaliya7@gmail.com>
2024-06-28 13:15:08 +0530

    add delete routes

--- backend/src/routes/chat-routes.ts
+++ backend/src/routes/chat-routes.ts
@@ -3,7 +3,9 @@ import { verifyToken } from "../utils/token-manager.js";
 import { chatCompletionValidator, validate } from "../utils/validators.js";
 
 import {
+  deleteChats,
   generateChatCompletion,
+  sendChatsToUser,
 } from "../controllers/chat-controllers.js";
 
 //Protected API
@@ -14,5 +16,7 @@ chatRoutes.post(
   verifyToken,
   generateChatCompletion
 );
+chatRoutes.get("/all-chats", verifyToken, sendChatsToUser);
+chatRoutes.delete("/delete", verifyToken, deleteChats);
 
 export default chatRoutes;
